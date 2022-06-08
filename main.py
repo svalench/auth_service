cebf2ed5e83873f7663a840409c1b5bbd365cd5c
valenchits <valenchits@mc-alutech24.com>
2022-06-08 14:46:49 +0300

    add index page

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
-from route import protected_user_model, router
-
+from fastapi import FastAPI, Request
+from core.route import protected_user_model, router
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get('/', response_class=HTMLResponse)
+def index_page(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 app.include_router(protected_user_model)
